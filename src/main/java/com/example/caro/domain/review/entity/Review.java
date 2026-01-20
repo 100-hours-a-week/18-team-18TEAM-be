f81e5a22675ef5202f670b8568d55a9290e41432
goddonggeon1234 <ristol@naver.com>
2026-01-20 23:52:15 +0900

    feat: add userdetails, chat, review entity

---
+++ src/main/java/com/example/caro/domain/review/entity/Review.java
@@ -0,0 +1,31 @@
+package com.example.caro.domain.review.entity;
+
+
+import com.example.caro.common.entity.BaseTimeEntity;
+import com.example.caro.domain.user.entity.User;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "review")
+public class Review extends BaseTimeEntity {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "reviewer_id", nullable = false)
+    private User reviewer;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "reviewee_id", nullable = false)
+    private User reviewee;
+
+    @Column(length = 500)
+    private String content;
+
+    @Column(nullable = false)
+    private Integer starScore; // TINYINT
+}
