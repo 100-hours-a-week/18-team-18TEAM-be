59b99773d913c3c6f2aa4cd07c110fcf346ed877
goddonggeon1234 <ristol@naver.com>
2026-01-23 13:36:53 +0900

    refactor: User entity 양방향 매핑 제거

--- src/main/java/com/caro/bizkit/domain/auth/dto/AccessTokenResponse.java
+++ src/main/java/com/caro/bizkit/domain/auth/dto/AccessTokenResponse.java
@@ -3,7 +3,6 @@ package com.caro.bizkit.domain.auth.dto;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 public record AccessTokenResponse(
-        @JsonProperty("access_token") String accessToken,
-        @JsonProperty("type") String type
+        @JsonProperty("access_token") String accessToken
 ) {
 }
